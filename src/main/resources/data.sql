0f5ff2179815203daa3c986cb7a6f00e6cd34d8c
pablochambi <pablochambi577@hotmail.com>
2024-09-24 11:56:47 -0300

    añadi un INSERT de cliente

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO Usuario(id, email, password, rol, activo, nombre, apellido, username) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true, 'nombreTest', 'apellidoTest', 'usernameTest');
+INSERT INTO Usuario(id, email, password, rol, activo, nombre, apellido, username) VALUES(null, 'cliente@gmail.com', '12345', 'CLIENTE', true, 'Leo', 'Messi', 'Leo Messi');
