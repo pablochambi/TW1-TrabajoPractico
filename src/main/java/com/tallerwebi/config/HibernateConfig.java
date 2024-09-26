69629f0268af881ba7d5e12278db68121fbe5c46
pablochambi <pablochambi577@hotmail.com>
2024-09-26 11:00:44 -0300

    se agrega la configuarion para mysql y una dependencia para que no se rompa

--- src/main/java/com/tallerwebi/config/HibernateConfig.java
+++ src/main/java/com/tallerwebi/config/HibernateConfig.java
@@ -16,11 +16,18 @@ public class HibernateConfig {
 
     @Bean
     public DataSource dataSource() {
+
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+//        dataSource.setUrl("jdbc:hsqldb:mem:db_");
+//        dataSource.setUsername("sa");
+//        dataSource.setPassword("");
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
-        dataSource.setUrl("jdbc:hsqldb:mem:db_");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/tallerweb1_2024");
+        dataSource.setUsername("root");
+        dataSource.setPassword("programacion");
         return dataSource;
     }
 
@@ -40,10 +47,16 @@ public class HibernateConfig {
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+
+        //properties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
-        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+
+        //properties.setProperty("hibernate.hbm2ddl.auto", "create");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+
         return properties;
     }
 }
