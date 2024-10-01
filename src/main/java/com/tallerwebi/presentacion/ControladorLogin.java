8a0ad6b06084fc24101a5af11a2cdcf20c78d259
pablochambi <pablochambi577@hotmail.com>
2024-10-01 19:01:01 -0300

    r

--- src/main/java/com/tallerwebi/presentacion/ControladorLogin.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorLogin.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-
 import javax.servlet.http.HttpServletRequest;
 @Controller
 public class ControladorLogin {
