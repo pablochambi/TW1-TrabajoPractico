531b6cec9b4b6d4367fcda86b95e8200c1ff0cf2
pablochambi <pablochambi577@hotmail.com>
2024-10-04 16:12:56 -0300

    e

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,5 @@
 -- ALTER TABLE archivo
 --     ADD CONSTRAINT FK_usuario_archivo FOREIGN KEY (usuario_id) REFERENCES usuario(id);
 
-INSERT INTO Usuario(id, email, password, rol, activo, nombre, apellido, username) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true, 'nombreTest', 'apellidoTest', 'usernameTest');
+INSERT IGNORE INTO Usuario(id, email, password, rol, activo, nombre, apellido, username) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true, 'nombreTest', 'apellidoTest', 'usernameTest');
 -- INSERT INTO Usuario(id, email, password, rol, activo, nombre, apellido, username) VALUES(null, 'cliente@gmail.com', '12345', 'CLIENTE', true, 'Leo', 'Messi', 'Leo Messi');
